fb4343ac08196da0e48211e2c9b0968ff61444a8
RAJESH ANDE <143387404+raju0005@users.noreply.github.com>
2024-12-12 13:37:21 +0530

    Update cloudinary.js

--- server/middlewares/cloudinary.js
+++ server/middlewares/cloudinary.js
@@ -2,19 +2,19 @@ import cloudinaryModule from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import dotenv from 'dotenv';
 
-// Load environment variables
+
 dotenv.config();
 
 const cloudinary = cloudinaryModule.v2;
 
-// Configure Cloudinary
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Configure Multer Storage with Cloudinary
+
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
