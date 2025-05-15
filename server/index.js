13b567a9de43005a9cec2c7cc83c46440db1f01e
raju0005 <anderajesh15@gmail.com>
2025-05-15 20:32:08 +0530

    Implementing Cors in Server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
-import dotenv from 'dotenv';
-import connectDB from './db/db.js';
-import profileRoutes from './routes/profileRoutes.js';
-import cors from 'cors';
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import profileRoutes from "./routes/profileRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -12,24 +12,32 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = [
+  "https://bynry-profiles.vercel.app",
+  "http://localhost:5173/",
+];
 
 const corsOptions = {
-  origin: 'https://bynry-profiles.vercel.app',
-  methods: 'GET,POST,PUT,DELETE',
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,POST,PUT,DELETE",
   credentials: true,
 };
 
-
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-
   res.send("Hello from Vercel!");
-})
+});
 
-app.use('/api/profiles', profileRoutes);
+app.use("/api/profiles", profileRoutes);
 
 app.listen(port, () => {
   console.log("Server running on http://localhost:5000");
